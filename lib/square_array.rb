3a37a23c95832b9b356ea6dfb82eba2318879940
George Apetrei <george.apetrei@gmail.com>
2019-12-16 13:53:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-counter =0
-while array[counter] do
-  array[counter] ** 2
-  counter +=1
-end
-puts array.join(",").split(" ")
+  counter = 0
+ 
+  while array[counter] do
+    puts array[counter] ** 2 
+    counter += 1
+  end
+  return array.join(" ")
 end
