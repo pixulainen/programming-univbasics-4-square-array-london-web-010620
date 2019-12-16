a1d52a02dc353cb946e21cd63dfca579fe085d37
George Apetrei <george.apetrei@gmail.com>
2019-12-16 13:17:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
-  square_array =Array.new(array.size)
-  while i<numbers.size
-      i = 0
-  square_array[i]= array[i]** 2
+i=0
+while i<array.length do
+  array[i] ** 2 
   i+=1
 end
-return square_array
+return array
 end
