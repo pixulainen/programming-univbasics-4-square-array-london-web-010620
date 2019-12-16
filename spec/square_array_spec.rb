4e77c767ef174914ff77fd173951e6d286e97dba
George Apetrei <george.apetrei@gmail.com>
2019-12-16 13:57:49 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -10,10 +10,10 @@ describe '#square_array' do
     expect(numbers).to_not receive(:inject)
     square_array(numbers)
   end
-
-  it 'should square the elements in an array' do
+it 'should square the elements in an array' do
     expect(square_array([1,2,3])).to eq([1,4,9])
     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
   end
 
 end
+  
